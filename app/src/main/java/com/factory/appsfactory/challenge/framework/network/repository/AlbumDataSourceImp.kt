0d6024772e4ca41d7733189b687f16888e0aaa2d
El7 <contact.elhassaniayoub@pm.me>
2022-02-13 22:57:05 +0100

    # Update network repository

--- app/src/main/java/com/factory/appsfactory/challenge/framework/network/repository/AlbumDataSourceImp.kt
+++ app/src/main/java/com/factory/appsfactory/challenge/framework/network/repository/AlbumDataSourceImp.kt
@@ -7,36 +7,47 @@ import com.factory.appsfactory.core.domain.AlbumDetails
 import com.factory.appsfactory.core.domain.Artist
 import com.factory.appsfactory.core.domain.Track
 
-class AlbumDataSourceImp (
+class AlbumDataSourceImp(
     private val lastFMService: LastFMService
-): RemoteAlbumDataSource {
+) : RemoteAlbumDataSource {
 
     override suspend fun getAlbumsByArtist(artist: Artist): List<Album> {
-        return lastFMService.getArtistAlbums(artist.name).topAlbums.map {
-            Album(
-                it.mbid,
-                it.name,
-                it.url,
-                "",
-                it.playcount
-            )
-        }
-    }
-
-    override suspend fun getAlbumDetails(album: Album, artist: Artist): AlbumDetails {
-        return with(lastFMService.getAlbumDetails(artist.name, album.name).albumInfo) {
-            val trackList = this.trackList?.map {
-                Track(
+        return takeIf { artist.name.isNotEmpty() }?.let {
+            lastFMService.getArtistAlbums(artist.name).topAlbums.map {
+                Album(
+                    it.mbid,
                     it.name,
                     it.url,
-                    it.duration
+                    "",
+                    it.playcount
                 )
-            } ?: emptyList()
-            AlbumDetails(
-                album,
-                artist,
-                trackList
-            )
+            }
+        } ?: emptyList()
+    }
+
+    override suspend fun getAlbumDetails(album: Album, artist: Artist): AlbumDetails? {
+        return takeIf { album.name.isNotEmpty() && artist.name.isNotEmpty() }?.let {
+            with(lastFMService.getAlbumDetails(artist.name, album.name).albumInfo) {
+                val trackList = this.trackList?.map {
+                    Track(
+                        it.name,
+                        it.url,
+                        it.duration
+                    )
+                } ?: emptyList()
+                val albumItem = Album(
+                    if (this.mbid.isEmpty()) album.id else this.mbid,
+                    if (this.albumName.isEmpty()) album.name else this.albumName,
+                    if (this.url.isEmpty()) album.url else this.url,
+                    "",
+                    if (this.playCount == 0L) album.playCount else this.playCount
+                )
+                AlbumDetails(
+                    albumItem,
+                    artist,
+                    trackList
+                )
+            }
         }
     }
 }
