7488f5ea15c30836f45c76974ef471d4ae03bfc0
El7 <contact.elhassaniayoub@pm.me>
2022-02-14 22:10:07 +0100

    # Make domain models serializable

--- core/src/main/java/com/factory/appsfactory/core/domain/Album.kt
+++ core/src/main/java/com/factory/appsfactory/core/domain/Album.kt
@@ -1,9 +1,11 @@
 package com.factory.appsfactory.core.domain
 
+import java.io.Serializable
+
 data class Album(
     val id: String,
     val name: String,
     val url: String,
     val thumbnail: String,
     val playCount: Long
-)
+): Serializable
--- core/src/main/java/com/factory/appsfactory/core/domain/Artist.kt
+++ core/src/main/java/com/factory/appsfactory/core/domain/Artist.kt
@@ -1,8 +1,10 @@
 package com.factory.appsfactory.core.domain
 
+import java.io.Serializable
+
 data class Artist(
     val id: String,
     val name: String,
     val url: String,
     val thumbnail: String
-)
+): Serializable
