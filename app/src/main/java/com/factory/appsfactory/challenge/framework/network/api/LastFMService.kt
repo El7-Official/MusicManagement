d405792368eb05f87d2405550b2b08ac775de65f
El7 <contact.elhassaniayoub@pm.me>
2022-02-14 02:55:52 +0100

    # Fix apikey value

--- app/src/main/java/com/factory/appsfactory/challenge/framework/network/api/LastFMService.kt
+++ app/src/main/java/com/factory/appsfactory/challenge/framework/network/api/LastFMService.kt
@@ -1,5 +1,6 @@
 package com.factory.appsfactory.challenge.framework.network.api
 
+import com.factory.appsfactory.challenge.BuildConfig
 import com.factory.appsfactory.challenge.framework.network.models.AlbumInfoResponseModel
 import com.factory.appsfactory.challenge.framework.network.models.AlbumsResponseModel
 import com.factory.appsfactory.challenge.framework.network.models.ArtistsResponseModel
@@ -8,22 +9,22 @@ import retrofit2.http.Query
 
 interface LastFMService {
 
-    @GET("method=artist.search&format=json")
+    @GET("?method=artist.search&format=json")
     suspend fun getArtistsByName(
         @Query("artist") artistName: String,
-        @Query("api_key") apiKey: String = ""
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): ArtistsResponseModel
 
-    @GET("method=artist.gettopalbums&format=json")
+    @GET("?method=artist.gettopalbums&format=json")
     suspend fun getArtistAlbums(
         @Query("artist") artistName: String,
-        @Query("api_key") apiKey: String = ""
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): AlbumsResponseModel
 
-    @GET("method=album.getinfo&format=json")
+    @GET("?method=album.getinfo&format=json")
     suspend fun getAlbumDetails(
         @Query("artist") artistName: String,
         @Query("album") albumName: String,
-        @Query("api_key") apiKey: String = ""
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): AlbumInfoResponseModel
 }
