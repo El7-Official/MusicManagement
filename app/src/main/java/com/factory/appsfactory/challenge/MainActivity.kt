22ec7b2b7c98b87b218722631fb11b725ecf2576
El7 <contact.elhassaniayoub@pm.me>
2022-02-14 22:48:32 +0100

    # Prepare navHost fragment

--- app/src/main/java/com/factory/appsfactory/challenge/MainActivity.kt
+++ app/src/main/java/com/factory/appsfactory/challenge/MainActivity.kt
@@ -2,10 +2,23 @@ package com.factory.appsfactory.challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.factory.appsfactory.challenge.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
