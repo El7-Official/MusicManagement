ff45ad149917e8615142d3bcdc1565a7ffc2995c
El7 <contact.elhassaniayoub@pm.me>
2022-02-14 13:37:53 +0100

    # Update artist dao

--- app/src/main/java/com/factory/appsfactory/challenge/framework/db/dao/ArtistDao.kt
+++ app/src/main/java/com/factory/appsfactory/challenge/framework/db/dao/ArtistDao.kt
@@ -3,10 +3,14 @@ package com.factory.appsfactory.challenge.framework.db.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
+import com.factory.appsfactory.challenge.framework.db.models.Artist
 import com.factory.appsfactory.challenge.framework.db.models.ArtistWithAlbums
 
 @Dao
 interface ArtistDao {
+
+    @Query("SELECT * FROM artists WHERE ref=:ref")
+    fun getArtistByRef(ref: String): Artist?
 
     @Transaction
     @Query("SELECT * FROM artists WHERE ref=:ref")
