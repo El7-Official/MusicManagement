a28ad70df2871f80a998ec71317c983eaea7608e
El7 <contact.elhassaniayoub@pm.me>
2022-02-14 03:23:38 +0100

    # Prepare base view model

---
+++ app/src/main/java/com/factory/appsfactory/challenge/presentation/viewmodel/BaseViewModel.kt
@@ -0,0 +1,27 @@
+package com.factory.appsfactory.challenge.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.factory.appsfactory.challenge.presentation.utils.CoroutineContextProvider
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel(val contextProvider: CoroutineContextProvider) : ViewModel() {
+
+    private val job: Job = Job()
+
+    abstract val coroutineExceptionHandler: CoroutineExceptionHandler
+
+    protected fun launchCoroutineIO(block: suspend CoroutineScope.() -> Unit): Job {
+        return viewModelScope.launch(contextProvider.io + job + coroutineExceptionHandler) {
+            block()
+        }
+    }
+
+    public override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+}
